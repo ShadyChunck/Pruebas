a8410af27a1acf34713c409c532f96457e1eaeb0
ShadyChunck <aalleexx1990@hotmail.com>
2026-03-05 15:53:37 -0600

    Autenticación Terminada (05-03-26)
    -Finalización de Autenticación de usuarios en todos los paneles y login.
    -Creación de "agregar_empleado(no usar, solo pruebas).html" que sirve para agregar usuarios/empleados/administradores. Solo con fines de desarrollo/experimentación.

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,4 +1,25 @@
 import { auth, db } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getDocs, collection, query, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const pagina = window.location.pathname.split("/").pop();
+
+onAuthStateChanged(auth, async (usuario) => {
+
+    if (!usuario)
+    {
+        if (pagina === "panel_configuracion.html") return window.location.href = "login.html";
+        if (pagina === "panel_dashboard.html") return window.location.href = "login.html";
+        if (pagina === "panel_inventario.html") return window.location.href = "login.html";
+        if (pagina === "panel_nueva-venta.html") return window.location.href = "login.html";
+        if (pagina === "panel_nuevo_producto.html") return window.location.href = "login.html";
+        if (pagina === "panel_reportes.html") return window.location.href = "login.html";
+        if (pagina === "panel_ventas.html") return window.location.href = "login.html";
+
+    } 
+
+});
+
+document.getElementById("cerrar_sesion").addEventListener("click", async () => {
+    await signOut(auth);
+});

f4f061f489661fd21530d9154959fda775f83901
ShadyChunck <aalleexx1990@hotmail.com>
2026-03-06 08:27:40 -0600

    Cambio de API

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAcjzoErPAkiarV3ozGkNlafqyX7IL8iH4",
-    authDomain: "autopartes-hessman.firebaseapp.com",
-    projectId: "autopartes-hessman",
-    storageBucket: "autopartes-hessman.firebasestorage.app",
-    messagingSenderId: "376837922284",
-    appId: "1:376837922284:web:ebee8b025e4538099034c9",
-    measurementId: "G-BKH5JHF9W2"
+    apiKey: "AIzaSyCK79FXbS3tNPL8V6C03HOMyrr8-rp6YO4",
+    authDomain: "autopartes-hessman-68283.firebaseapp.com",
+    projectId: "autopartes-hessman-68283",
+    storageBucket: "autopartes-hessman-68283.firebasestorage.app",
+    messagingSenderId: "854961822123",
+    appId: "1:854961822123:web:75d5b5d3d3e567761e9aee",
+    measurementId: "G-ENNB0G1JME"
 };
 
 // Initialize Firebase
